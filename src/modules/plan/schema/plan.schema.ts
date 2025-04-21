db550746bba844671f028abb78a3a8aa8f6a8140
gyx <gyx19971018@outlook.com>
2025-04-21 17:39:04 +1000

    complete plan module

--- src/modules/plan/schema/plan.schema.ts
+++ src/modules/plan/schema/plan.schema.ts
@@ -1,28 +1,17 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
-import { ApiProperty } from '@nestjs/swagger';
 
 export type PlanDocument = HydratedDocument<Plan>;
 
 @Schema()
 export class Plan {
 
-  @ApiProperty({ description: 'Plan name', example: 'Free' })
   @Prop({ required: true, unique: true })
   name: string;
 
-  @ApiProperty({ description: 'Plan tier', example: 'FREE | BASIC | PRO' })
   @Prop({ required: true, enum: ['FREE', 'BASIC', 'PRO'] })
   tier: 'FREE' | 'BASIC' | 'PRO';
 
-  @ApiProperty({
-    description: 'Pricing options with flexible billing using rrule format',
-    example: [
-      { rrule: 'FREQ=MONTHLY;INTERVAL=1', price: 49 },
-      { rrule: 'FREQ=MONTHLY;INTERVAL=3', price: 129 },
-      { rrule: 'FREQ=YEARLY;INTERVAL=1', price: 499 }
-    ]
-  })
   @Prop({
     type: [
       {
@@ -37,10 +26,6 @@ export class Plan {
     price: number;
   }[];
 
-  @ApiProperty({ description: 'Plan features', example: {
-    callMinutes: 'Unlimited',
-    support: 'Basic support'
-  }})
   @Prop({
     type: {
       callMinutes: { type: String, required: true },
@@ -53,7 +38,6 @@ export class Plan {
     support: string;
   };
 
-  @ApiProperty({ description: 'Plan is active or not', example: true })
   @Prop({ default: true })
   isActive: boolean;
 }
