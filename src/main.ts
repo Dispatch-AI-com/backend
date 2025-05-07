8df6724593bc0529ca040b35a779afca08abe751
Depeng Sun <shangguming@gmail.com>
2025-05-07 09:54:25 +0930

    update format (#25)

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
+import { setupSwagger } from '@/config/swagger.config';
 import { AppModule } from '@/modules/app.module';
 
 async function bootstrap(): Promise<void> {
@@ -12,6 +13,8 @@ async function bootstrap(): Promise<void> {
   app.enableCors({
     origin: process.env.CORS_ORIGIN ?? '*',
   });
+
+  setupSwagger(app);
 
   const port = process.env.PORT ?? 4000;
   await app.listen(port);
