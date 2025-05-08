2c111cbff7050b3b95ef07ebbbf54800cdfb79fb
Depeng Sun <shangguming@gmail.com>
2025-05-08 23:42:23 +0930

    done

--- src/main.ts
+++ src/main.ts
@@ -10,15 +10,9 @@ import { AppModule } from '@/modules/app.module';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.useLogger(winstonLogger);
-  Logger.error('🚨 This is a test error log from Dispatch AI');
-  Logger.warn('🚨 This is a test warn log from Dispatch AI');
-  Logger.log('🚨 This is a test log from Dispatch AI');
-  Logger.debug('🚨 This is a test debug log from Dispatch AI');
-  Logger.verbose('🚨 This is a test verbose log from Dispatch AI');
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-  Logger.log('Hello from Dispatch AI logger!');
   app.enableCors({
     origin: process.env.CORS_ORIGIN ?? '*',
   });
