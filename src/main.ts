0d180313c524a329296b38e281e2579516c779f2
gyx <gyx19971018@outlook.com>
2025-05-20 17:48:05 +1000

    feat: initial subscription and stripe module

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import morgan from 'morgan';
+import * as express from 'express';
 
 import { setupSwagger } from '@/config/swagger.config';
 import { winstonLogger } from '@/logger/winston.logger';
@@ -10,7 +11,7 @@ import { AppModule } from '@/modules/app.module';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.useLogger(winstonLogger);
-
+  app.use('/api/webhooks/stripe', express.raw({ type: 'application/json' }));
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -5,6 +5,8 @@ import { AuthModule } from '@/modules/auth/auth.module';
 import { DatabaseModule } from '@/modules/database/database.module';
 import { HealthModule } from '@/modules/health/health.module';
 import { PlanModule } from '@/modules/plan/plan.module';
+import { SubscriptionModule } from '@/modules/subscription/subscription.module';
+import { StripeModule } from '@/modules/stripe/stripe.module';
 
 @Module({
   imports: [
@@ -15,6 +17,8 @@ import { PlanModule } from '@/modules/plan/plan.module';
     HealthModule,
     AuthModule,
     PlanModule,
+    SubscriptionModule,
+    StripeModule,
   ],
 })
 export class AppModule {}
