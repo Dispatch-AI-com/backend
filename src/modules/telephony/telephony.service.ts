590b68573c0102f14ab773345aa23edf6d00f916
Mark Wang <markmagicwang@gmail.com>
2025-07-30 18:05:06 +1000

    default message for serivce booking note

--- src/modules/telephony/telephony.service.ts
+++ src/modules/telephony/telephony.service.ts
@@ -390,7 +390,7 @@ export class TelephonyService {
       }
     }
 
-    // 构建service booking数据
+    // create service booking data
     const serviceBookingData: CreateServiceBookingDto = {
       serviceId: session.user.service.id,
       client: {
